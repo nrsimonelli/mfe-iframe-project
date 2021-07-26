f5dca947264aa39401279c0ee9189e5da7d1ecc0
nick.simonelli <nick.simonelli@calabrio.com>
2021-07-26 13:58:19 -0500

    test will search header

---
+++ src/root.component.js
@@ -0,0 +1,37 @@
+import { RiSailboatFill } from "react-icons/ri";
+import { FaKiwiBird } from "react-icons/fa";
+import { BsGearFill } from "react-icons/bs";
+
+export default function Root(props) {
+  return (
+    <main>
+      <section>
+        <h1>Hello spicy nuggets</h1>
+        <div>This is a single page application</div>
+        <div>
+          Otherwise known as <span className="gradient-text">spa.</span>
+        </div>
+      </section>
+      <section className="buffer">
+        <div className="card-title">Advantages include...</div>
+        <div className="card-section">
+          <div className="card-container">
+            <FaKiwiBird className="card-image" />
+            <div className="card-text">Agnostic</div>
+          </div>
+          <div className="card-container">
+            <RiSailboatFill className="card-image" />
+            <div className="card-text">Nimble</div>
+          </div>
+          <div className="card-container">
+            <BsGearFill className="card-image" />
+            <div className="card-text">Autonomus</div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="button">more</div>
+      </section>
+    </main>
+  );
+}
